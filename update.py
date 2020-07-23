62a92e1a32274f41c3f1d9c4918ab3ed41a0ef44
kaminosama <417690600@qq.com>
2020-07-23 15:30:53 +0800

    fix some issues

--- update.py
+++ update.py
@@ -5,8 +5,11 @@ import shutil
 if __name__ == '__main__':
     os.chdir("/home/adidev/adidev")
     os.system("git pull")
+    origin_path = "/home/adidev/adidev"
     for filename in os.listdir("/home/adidev"):
         if filename.startswith("adidev_"):
+            factory_code = filename.split("_")[1]
+            new_path = f"/home/adidev/adidev_{factory_code}"
             shutil.move(
                 f"/home/adidev/{filename}/filter/config.py",
                 f"/home/adidev/config_{filename.split('_')[1]}.py"
@@ -17,4 +20,8 @@ if __name__ == '__main__':
                 f"/home/adidev/config_{filename.split('_')[1]}.py",
                 f"/home/adidev/{filename}/filter/config.py"
             )
+            with open(f"/home/adidev/{filename}/deploy/filter.bash", "w") as w:
+                with open(f"/home/adidev/adidev/deploy/filter.bash.sample", "r") as r:
+                    w.write(r.read().format(factory_code=factory_code))
+            os.system(f"chmod +x {new_path}/deploy/filter.bash")
     os.system("supervisorctl reload")
